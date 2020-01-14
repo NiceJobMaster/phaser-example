d6a3fcdbd141bff0523b4b240dc64c2b9bc6a227
unknown <warardo@gmail.com>
2020-01-14 20:32:19 +0100

    phaser

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import * as Phaser from 'phaser'
+import Scenes from './scenes'
+
+const config: Phaser.Types.Core.GameConfig = {
+  type: Phaser.AUTO,
+  width: window.innerWidth,
+  height: window.innerHeight,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 300 },
+      debug: false,
+    },
+  },
+  scene: Scenes,
+}
+
+export const game = new Phaser.Game(config)
+
+window.addEventListener('resize', () => {
+  game.scale.refresh()
+})
--- webpack/base.js
+++ webpack/base.js
@@ -1,56 +1,59 @@
-const webpack = require("webpack");
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const webpack = require('webpack')
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
-  entry: "./src/index.js",
+  mode: 'development',
+  devtool: 'eval-source-map',
+  entry: './src/index.ts',
   output: {
-    path: path.resolve("dist"),
-    filename: "index_bundle.js"
+    path: path.resolve('dist'),
+    filename: 'index_bundle.js',
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [{ loader: "style-loader" }, { loader: "css-loader" }]
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader"
-        }
+          loader: 'babel-loader',
+        },
       },
       {
-        test: /\.jsx?$/,
+        test: /\.tsx?$/,
+        use: 'ts-loader',
         exclude: /node_modules/,
-        use: "babel-loader"
       },
       {
         test: [/\.vert$/, /\.frag$/],
-        use: "raw-loader"
+        use: 'raw-loader',
       },
       {
         test: /\.(gif|png|jpe?g|svg|xml)$/i,
-        use: "file-loader"
-      }
-    ]
+        use: 'file-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new CleanWebpackPlugin({
-      root: path.resolve(__dirname, "../")
+      root: path.resolve(__dirname, '../'),
     }),
     new webpack.DefinePlugin({
       CANVAS_RENDERER: JSON.stringify(true),
-      WEBGL_RENDERER: JSON.stringify(true)
+      WEBGL_RENDERER: JSON.stringify(true),
     }),
     new HtmlWebpackPlugin({
-      template: "./index.html",
-      filename: "index.html",
-      inject: "body"
-    })
-  ]
-};
+      template: './index.html',
+      filename: 'index.html',
+      inject: 'body',
+    }),
+  ],
+}
